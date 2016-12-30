effc6258daf2e0d810aed6087020be702666bea8
scherbinanikolaj <scherbinanikolaj@gmail.com>
2016-12-30 14:31:49 +0200

    add addition

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,9 @@ public class Calculate {
     public static void main(String[] args)
     {
 	System.out.println("Calculate");
+	double first = Double.valueOf(args[0]);
+	double second = Double.valueOf(args[1]);
+	double summ = first * second;
+	System.out.println("summ = " + summ);
     }
 }
